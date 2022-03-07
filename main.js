0aadd79499b834d9f183dcd9356e9f9c9ae2d571
unknown <the239117@gmail.com>
2022-03-07 20:44:49 +0700

    first verson

---
+++ main.js
@@ -0,0 +1,50 @@
+
+const btnSuccess = document.querySelector('.btn-success');
+const btnRun = document.querySelector('.btn-run');
+const title = document.querySelector('.title');
+const desc = document.querySelector('.desc');
+
+const form = document.querySelector('.form');
+const btnSubmit = document.querySelector('.btn-submit');
+const yourName = document.querySelector('#your-name');
+const friendName = document.querySelector('#friend-name');
+
+btnSubmit.onclick = function () {
+    if (yourName.value && friendName.value) {
+        addYourName(yourName.value);
+        addFriendName(friendName.value);
+        hideForm();
+    };
+
+};
+
+function hideForm() {
+    form.classList.add('hide');
+};
+
+function addYourName(name) {
+    btnSuccess.innerText = name;
+};
+
+function addFriendName(name) {
+    btnRun.innerText = name;
+};
+
+btnRun.onmouseover = function () {
+    let newX = Math.floor(Math.random() * 100);
+    let newY = Math.floor(Math.random() * 100);
+    console.log(newX, newY);
+    btnRun.style.top = newX + '%';
+    btnRun.style.left = newY + '%';
+    desc.innerText = 'Sai rồi xem bạn êi =))';
+};
+
+btnSuccess.onmouseover = function () {
+    desc.innerText = 'Đoán xem =)))';
+};
+
+btnSuccess.onclick = function (e) {
+    desc.innerText = 'Congratulation';
+    alert('Wao! Giờ biết ai là Heo rồi nhé :))');
+    location.reload();
+};
